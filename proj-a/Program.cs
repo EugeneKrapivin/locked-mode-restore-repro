b57db4ed1c4f43b5e84fb5b24a737329058a5481
Eugene Krapivin <eugene.krapivin@sap.com>
2020-11-16 23:19:23 +0200

    initial

---
+++ proj-a/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using proj_b;
+
+namespace proj_a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = new Class{
+                Message = "repro?!"
+            };
+            Console.WriteLine(c.GetMessage());
+        }
+    }
+}
---
+++ proj-b/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace proj_b
+{
+    public class Class
+    {
+        public string Message { get; set; } = "no message";
+
+        public string GetMessage() => $"[{DateTime.UtcNow:o}] - {Message}";
+    }
+}
